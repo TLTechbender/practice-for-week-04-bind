698b40c700a738ad448d004be38b7411db7b987d
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-10 16:39:19 +0100

    copied and Feat: Did some myself, got confused later and ended up copying the answer solution

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
+const Employee= require("./employee");
 // Your code here
+//
+const john = new Employee("John Wick","Dog Lover");
+
+/*It was at this point that a fella knew, he was stranded*/
+
+
+setTimeout(john.sayName, 2000); // => undefined says hello
+const sayHello = john.sayName;
+const johnSaysHello = sayHello.bind(john);
+
+setTimeout(johnSaysHello, 2000); // => John Wick says hello
+
+setTimeout(john.sayOccupation, 3000); // => undefined is a undefined
+const sayOccupation = john.sayOccupation;
+const johnSaysOccupation = sayOccupation.bind(john);
+
+setTimeout(johnSaysOccupation, 3000);
